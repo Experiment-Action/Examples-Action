10ef8d82d0e5393d41c9b4d7c0d0e61da96e1d56
yuj0607 <yuj0607@163.com>
2022-07-26 09:11:24 +0800

    Update output.py

--- .github/output.py
+++ .github/output.py
@@ -9,7 +9,8 @@ def out_markdown(content):
     for line in content:
         markdown = markdown + """%s\%0A""" % line
     #print(f"::set-output name=report::{markdown}")
-    print(f"::set-output name=msg::{markdown}")
+    txt = "afswewtw"
+    print(f"::set-output name=msg::{txt}")
     return markdown
     
     
