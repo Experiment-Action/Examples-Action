16a0c455e6c1957f0822bf4cb2d99e8a54839340
fish9167 <674821954@qq.com>
2022-07-26 08:53:05 +0800

    Create output.py

---
+++ .github/output.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+def out_markdown(content):
+    markdown = """
+| Platform | build | runned | result | FailScene |
+| :---: | :---: | :---: | :---: | :---: |
+"""
+    for line in content:
+        markdown = markdown + """%s
+""" % line
+    print(f"::set-output name=report::{markdown}")
+    return markdown
+    
+    
+    
+    
+content = ['| 1 | 4 | 5 | 353 | 43535 | ', '| 567575 | 3535 | 5675 | dgds | ghfs | '  ]
+out_markdown(content)
