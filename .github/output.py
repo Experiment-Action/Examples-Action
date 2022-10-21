c7b621e7388a6a2cca0b27174e79a10fdfc06f35
yuj0607 <yuj0607@163.com>
2022-10-21 16:53:12 +0800

    Update output.py

--- .github/output.py
+++ .github/output.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import os
-env_file = os.getenv('GITHUB_OUTPUT')
+env_file = os.getenv('GITHUB_ENV')
 hello='^0^ 我是测试的 ^0^'
 
 
@@ -15,13 +15,11 @@ def out_markdown(content):
     #print(f"::set-output name=msg::{txt}")
     return markdown
     
-    #with open(env_file, "a") as f:
-    #    f.write(f"details={markdown}")
-    #   f.write(f"msg={txt}")
-    #    f.write(f"wahah={hello}")
 
+    with open(env_file, "a") as myfile:
+        myfile.write("details=markdown\n")
+        myfile.write("msg=txt")
 
-os.environ['GITHUB_OUTPUT'] = f"output_matrix = {hello}"
 
 content = ['| 1 | 4 | 5❌ | 353 | 43535✅ | ', '| 567575 | 3535 | 5675 | [dgds](http://lan-jenkins.cocos.org/job/CocosGitHub/job/PR_Action/job/windows/682/artifact/image/compareimage-android-group01/advance_widget_startplay_1.png) | ghfs ☑ | '  ]
 out_markdown(content)
