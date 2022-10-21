fba4f4418b51457c1391f1b2b8950a54a22484f5
yuj0607 <yuj0607@163.com>
2022-10-21 16:30:03 +0800

    Update output.py

--- .github/output.py
+++ .github/output.py
@@ -4,20 +4,20 @@ import os
 env_file = os.getenv('GITHUB_ENV')
 hello='hello'
 
-with open(env_file, "a") as myfile:
-    myfile.write(f"wahah={hello}")
 
 def out_markdown(content):
     markdown = """%0A%0A| Platform | build | runned | result | FailScene | %0A| :---: | :---: | :---: | :---: | :---: | %0A"""
 
     for line in content:
         markdown = markdown + line + '%0A'
-    print(f"::set-output name=details::{markdown}")
+    #print(f"::set-output name=details::{markdown}")
     txt = "afswewtw"
-    print(f"::set-output name=msg::{txt}")
+    #print(f"::set-output name=msg::{txt}")
     return markdown
     
-    
+    with open(env_file, "a") as f:
+        f.write(f"details={markdown}")
+        f.write(f"msg={txt}")
     
     
 content = ['| 1 | 4 | 5❌ | 353 | 43535✅ | ', '| 567575 | 3535 | 5675 | [dgds](http://lan-jenkins.cocos.org/job/CocosGitHub/job/PR_Action/job/windows/682/artifact/image/compareimage-android-group01/advance_widget_startplay_1.png) | ghfs ☑ | '  ]
