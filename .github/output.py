359f08ac201c3f5c8dbde63dcb465750062a35d7
yuj0607 <yuj0607@163.com>
2022-10-21 17:22:24 +0800

    Update output.py

--- .github/output.py
+++ .github/output.py
@@ -9,10 +9,10 @@ hello='^0^ 我是测试的 ^0^'
 
 
 def out_markdown(content):
-    markdown = '| Platform | build | runned | result | FailScene | %0A| :---: | :---: | :---: | :---: | :---: | '
+    markdown = '| Platform | build | runned | result | FailScene | \n | :---: | :---: | :---: | :---: | :---: | '
 
     for line in content:
-        markdown = markdown + line + '%0A'
+        markdown = markdown + line + '\n'
     #print(f"::set-output name=details::{markdown}")
     txt = "afswewtw"
     #print(f"::set-output name=msg::{txt}")
